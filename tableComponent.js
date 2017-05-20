15fe44d9fdd6107be3c755fa224dfc7dab17cc24
Mateusz Zajac <zjacer@gmail.com>
2017-05-20 21:59:18 +0200

    xkom parser reworked

--- tableComponent.js
+++ tableComponent.js
@@ -1,56 +1,26 @@
-function getEndingTime() { 
-    now = new Date();
-    nowTime = now.getHours()*3600 + now.getMinutes()*60 + now.getSeconds();
-    
-    // nowTime - morning promotion hour (10:00:00) and nowTime - evening promotion hour (22:00:00)
-    return (nowTime - 36000 > 79200 - nowTime) ? "10:00" : "22:00";
-}
-
 xkomModule = angular.module('occasionProvider', []);
     xkomModule.component('tableData', {
         templateUrl: 'tableTemplate.html'
 });
-// TODO: merge xpath conditions!!!!
 
-// X-KOM.PL SHOP
 xkomData = angular.module('occasionProvider');
 xkomData.controller('_xkomdata', function($scope, $http) {
-    $scope.xkomArrayData = new Array(5);
+    $scope.xkomArrayData = new Array(4);
     
-    // product image
     $http({
         method: 'GET',
         // select * from html where url='https://www.x-kom.pl' and xpath='//*[@id="hotShot"]/div[2]/div[1]'
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fwww.x-kom.pl%2F\'%20and%20xpath%3D\'%2F%2F*%5B%40id%3D%22hotShot%22%5D%2Fdiv%5B2%5D%2Fdiv%5B1%5D%2Fimg\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fx-kom.pl\'%20and%20xpath%3D\'%2F%2Fdiv%5Bcontains(%40class%2C%20%22hot-shot%22)%5D%2Fdiv%5Bcontains(%40class%2C%20%22row%22)%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     }).then( function(result) {
-        $scope.xkomData = result.data.query;
-        $scope.xkomArrayData[0] = $scope.xkomData.results.img.src;
-    });
-    
-    // product name
-    $http({
-        method: 'GET',
-        // select * from html where url='https://www.x-kom.pl' and xpath='//*[@id="hotShot"]/div[2]/div[1]/p'
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'http%3A%2F%2Fx-kom.pl\'%20and%20xpath%3D\'%2F%2F*%5B%40id%3D%22hotShot%22%5D%2Fdiv%5B2%5D%2Fdiv%5B1%5D%2Fp\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    }).then( function(result) {
-        $scope.xkomData = result.data.query;
-        $scope.xkomArrayData[1] = $scope.xkomData.results.p.content;
-    });
-    
-    $scope.xkomArrayData[2] = getEndingTime();
-    // old and new price
-    $http({
-        method: 'GET',
-        // select * from html where url='https://www.x-kom.pl' and xpath='//*[@id="hotShot"]/div[2]/div[2]/div[1]'
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fwww.x-kom.pl\'%20and%20xpath%3D\'%2F%2F*%5B%40id%3D%22hotShot%22%5D%2Fdiv%5B2%5D%2Fdiv%5B2%5D%2Fdiv%5B1%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    }).then( function(result) {
-        $scope.xkomData = result.data.query.results.div.div;
-        $scope.xkomArrayData[3] = $scope.xkomData[0].content;
-        $scope.xkomArrayData[4] = $scope.xkomData[1].content;
+        $scope.xkomData = result.data.query.results.div[1];
+        $scope.xkomArrayData[0] = $scope.xkomData.div[0].img.src;
+        $scope.xkomArrayData[1] = $scope.xkomData.div["0"]["data-product-name"]
+        $scope.xkomArrayData[2] = $scope.xkomData.div[1].div["0"].div["0"].content;
+        $scope.xkomArrayData[3] = $scope.xkomData.div[1].div["0"].div[1].content;
     });
 });
 
-// AL.TO SHOP
+// AL.TO SHOP - to rework
 altoData = angular.module('occasionProvider');
 altoData.controller('_altodata', function($scope, $http) {
     $scope.altoArrayData = new Array(5);
@@ -76,7 +46,6 @@ altoData.controller('_altodata', function($scope, $http) {
         $scope.altoArrayData[1] = $scope.altoData.results.p.content;
     });
     
-    $scope.altoArrayData[2] = getEndingTime();
     // old and new price
     $http({
         method: 'GET',
@@ -116,16 +85,3 @@ moreleData.controller('moreleController', function($scope, $http) {
 		};
 	});
 });
-    
-    
-
-//komputronikData = angular.module('occasionProvider');
-//komputronikData.controller('_moreledata', function($scope, $http) {
-//    $scope.moreleArrayData = new Array(5);
-
-// RTV EURO AGD SHOP - Dla Wasia, masz tu juz zalazek
-//rtveuroagdData = angular.module('occasionProvider');
-//rtveuroagdData.controller('_rtveuroagddata', function($scope, $http) {
-//    $scope.rtveuroagdArrayData = new Array(5);
-
-// + troche roboty dla ciebie, ktora ci opisze w wiadomosci, bo tutaj to masz pikus, nie robote
