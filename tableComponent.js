9026ff825fd26bbb82a097747c59c24a602ca629
Mateusz Zajac <zjacer@gmail.com>
2017-05-20 23:36:24 +0200

    al.to parser reworked, code fixes & price reduction calculator

--- tableComponent.js
+++ tableComponent.js
@@ -1,15 +1,29 @@
+function calculateReduction(originalPrice, reducedPrice) {
+    originalPrice = Number(originalPrice.replace(/[^0-9\.]+/g, ""));
+    reducedPrice = Number(reducedPrice.replace(/[^0-9\.]+/g, ""));
+    return Math.floor(100 - ((reducedPrice * 100) / originalPrice));
+}
+
+/* ARRAY DATA STRUCTURE
+[0] - item image src
+[1] - item name
+[2] - item price
+[3] - item price after reduction
+*/
+
 xkomModule = angular.module('occasionProvider', []);
     xkomModule.component('tableData', {
         templateUrl: 'tableTemplate.html'
 });
 
+// X-KOM.PL
 xkomData = angular.module('occasionProvider');
-xkomData.controller('_xkomdata', function($scope, $http) {
-    $scope.xkomArrayData = new Array(4);
+xkomData.controller('xkomController', function($scope, $http) {
+    $scope.xkomArrayData = new Array(5);
     
     $http({
         method: 'GET',
-        // select * from html where url='https://www.x-kom.pl' and xpath='//*[@id="hotShot"]/div[2]/div[1]'
+        // select * from html where url='https://x-kom.pl' and xpath='//div[contains(@class, "hot-shot")]/div[contains(@class, "row")]'
         url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fx-kom.pl\'%20and%20xpath%3D\'%2F%2Fdiv%5Bcontains(%40class%2C%20%22hot-shot%22)%5D%2Fdiv%5Bcontains(%40class%2C%20%22row%22)%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     }).then( function(result) {
         $scope.xkomData = result.data.query.results.div[1];
@@ -17,47 +31,12 @@ xkomData.controller('_xkomdata', function($scope, $http) {
         $scope.xkomArrayData[1] = $scope.xkomData.div["0"]["data-product-name"]
         $scope.xkomArrayData[2] = $scope.xkomData.div[1].div["0"].div["0"].content;
         $scope.xkomArrayData[3] = $scope.xkomData.div[1].div["0"].div[1].content;
+        $scope.xkomArrayData[4] = calculateReduction($scope.xkomArrayData[2], $scope.xkomArrayData[3]);
     });
+
 });
 
-// AL.TO SHOP - to rework
-altoData = angular.module('occasionProvider');
-altoData.controller('_altodata', function($scope, $http) {
-    $scope.altoArrayData = new Array(5);
-    
-    // product image
-    $http({
-        method: 'GET',
-        // select * from html where url='https://www.al.to' and xpath='//*[@id="hotShot"]/div[2]/div[1]'
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fwww.al.to%2F\'%20and%20xpath%3D\'%2F%2F*%5B%40id%3D%22hotShot%22%5D%2Fdiv%5B2%5D%2Fdiv%5B1%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    }).then( function(result) {
-        $scope.altoData = result.data.query;
-        $scope.altoArrayData[0] = $scope.altoData.results.div.img.src;
-        console.log($scope.altoArrayData[0]);
-    });
-    
-    // product name
-    $http({
-        method: 'GET',
-        // select * from html where url='https://www.al.to' and xpath='//*[@id="hotShot"]/div[2]/div[1]/p'
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fwww.al.to%2F\'%20and%20xpath%3D\'%2F%2F*%5B%40id%3D%22hotShot%22%5D%2Fdiv%5B2%5D%2Fdiv%5B1%5D%2Fp\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    }).then( function(result) {
-        $scope.altoData = result.data.query;
-        $scope.altoArrayData[1] = $scope.altoData.results.p.content;
-    });
-    
-    // old and new price
-    $http({
-        method: 'GET',
-        // select * from html where url='https://www.al.to' and xpath='//*[@id="hotShot"]/div[2]/div[2]/div[1]'
-        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fwww.al.to%2F\'%20and%20xpath%3D\'%2F%2F*%5B%40id%3D%22hotShot%22%5D%2Fdiv%5B2%5D%2Fdiv%5B2%5D%2Fdiv%5B1%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-    }).then( function(result) {
-        $scope.altoData = result.data.query.results.div.div;
-        $scope.altoArrayData[3] = $scope.altoData[0].content;
-        $scope.altoArrayData[4] = $scope.altoData[1].content;
-    });
-});
-
+// MORELE.NET
 moreleData = angular.module('occasionProvider');
 moreleData.controller('moreleController', function($scope, $http) {
 	
@@ -71,7 +50,6 @@ moreleData.controller('moreleController', function($scope, $http) {
 		var productName = productModel.div[0];
 		var productDescription = productModel.div[1];
 		var productPrice = productModel.div[2].div;
-		
 		var startIndexUrl = imgModel.style.indexOf("https");
 		var endIndexUrl = imgModel.style.indexOf("jpg") + 3;
 		
@@ -84,4 +62,23 @@ moreleData.controller('moreleController', function($scope, $http) {
 			NewPrice: productPrice.div[1].span
 		};
 	});
+});
+
+// AL.TO
+altoData = angular.module('occasionProvider');
+altoData.controller('altoController', function($scope, $http) {
+    $scope.altoArrayData = new Array(4);
+    
+    $http({
+        method: 'GET',
+        // select * from html where url='https://al.to' and xpath='//div[contains(@class, "hot-shot")]/div[contains(@class, "row")]'
+        url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fal.to\'%20and%20xpath%3D\'%2F%2Fdiv%5Bcontains(%40class%2C%20%22hot-shot%22)%5D%2Fdiv%5Bcontains(%40class%2C%20%22row%22)%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    }).then( function(result) {
+        $scope.altoData = result.data.query.results.div[1];
+        $scope.altoArrayData[0] = $scope.altoData.div[0].img.src;
+        $scope.altoArrayData[1] = $scope.altoData.div["0"]["data-product-name"]
+        $scope.altoArrayData[2] = $scope.altoData.div[1].div["0"].div["0"].content;
+        $scope.altoArrayData[3] = $scope.altoData.div[1].div["0"].div[1].content;
+        $scope.altoArrayData[4] = calculateReduction($scope.altoArrayData[2], $scope.altoArrayData[3]);
+    });
 });
