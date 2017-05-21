aeaa14434ff8931ea023f3f6ad787e04414bbb96
Mateusz Zajac <zjacer@gmail.com>
2017-05-21 19:03:01 +0200

    amfora.pl parser and some code tweaks

--- tableComponent.js
+++ tableComponent.js
@@ -10,6 +10,7 @@ function calculateReduction(originalPrice, reducedPrice) {
 [2] - item price
 [3] - item price after reduction
 [4] - % reduction
+[5] - item url
 */
 
 xkomModule = angular.module('occasionProvider', []);
@@ -20,7 +21,7 @@ xkomModule = angular.module('occasionProvider', []);
 // X-KOM.PL
 xkomData = angular.module('occasionProvider');
 xkomData.controller('xkomController', function($scope, $http) {
-    $scope.xkomArrayData = new Array(5);
+    $scope.xkomArrayData = [];
     
     $http({
         method: 'GET',
@@ -28,6 +29,7 @@ xkomData.controller('xkomController', function($scope, $http) {
         url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fx-kom.pl\'%20and%20xpath%3D\'%2F%2Fdiv%5Bcontains(%40class%2C%20%22hot-shot%22)%5D%2Fdiv%5Bcontains(%40class%2C%20%22row%22)%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     }).then( function(result) {
         $scope.xkomData = result.data.query.results.div[1];
+        
         $scope.xkomArrayData[0] = $scope.xkomData.div[0].img.src;
         $scope.xkomArrayData[1] = $scope.xkomData.div["0"]["data-product-name"]
         $scope.xkomArrayData[2] = $scope.xkomData.div[1].div["0"].div["0"].content;
@@ -68,7 +70,7 @@ moreleData.controller('moreleController', function($scope, $http) {
 // AL.TO
 altoData = angular.module('occasionProvider');
 altoData.controller('altoController', function($scope, $http) {
-    $scope.altoArrayData = new Array(4);
+    $scope.altoArrayData = [];
     
     $http({
         method: 'GET',
@@ -76,10 +78,35 @@ altoData.controller('altoController', function($scope, $http) {
         url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fal.to\'%20and%20xpath%3D\'%2F%2Fdiv%5Bcontains(%40class%2C%20%22hot-shot%22)%5D%2Fdiv%5Bcontains(%40class%2C%20%22row%22)%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
     }).then( function(result) {
         $scope.altoData = result.data.query.results.div[1];
+        
         $scope.altoArrayData[0] = $scope.altoData.div[0].img.src;
         $scope.altoArrayData[1] = $scope.altoData.div["0"]["data-product-name"]
         $scope.altoArrayData[2] = $scope.altoData.div[1].div["0"].div["0"].content;
         $scope.altoArrayData[3] = $scope.altoData.div[1].div["0"].div[1].content;
         $scope.altoArrayData[4] = calculateReduction($scope.altoArrayData[2], $scope.altoArrayData[3]);
+    });
+});
+
+// AMFORA.PL
+amforaData = angular.module('occasionProvider');
+amforaData.controller('amforaController', function($scope, $http) {
+    $scope.amforaArrayData = [];
+    
+    $http({
+        method: 'GET',
+        // select * from html where url='https://www.amfora.pl/' and xpath='//div[contains(@class, "top-wrap")]/div[contains(@class, "row")]'
+		url: 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20html%20where%20url%3D\'https%3A%2F%2Fwww.amfora.pl%2F\'%20and%20xpath%3D\'%2F%2Fdiv%5Bcontains(%40class%2C%20%22top-wrap%22)%5D%2Fdiv%5Bcontains(%40class%2C%20%22row%22)%5D\'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+    }).then( function(result) {
+        $scope.amforaData = result.data.query.results.div;
+        
+        $scope.imgUrl = $scope.amforaData.div["0"].div.a.style;
+        $scope.urlStart = $scope.imgUrl.indexOf("https");
+        $scope.urlEnd = $scope.imgUrl.indexOf("jpeg") + 4;
+        
+        $scope.amforaArrayData[0] = $scope.imgUrl.substring($scope.urlStart, $scope.urlEnd);
+        $scope.amforaArrayData[1] = $scope.amforaData.div[1].div["0"].a.title
+        $scope.amforaArrayData[2] = $scope.amforaData.div[1].div[2].div.div["0"].span;
+        $scope.amforaArrayData[3] = $scope.amforaData.div[1].div[2].div.div[1].span;
+        $scope.amforaArrayData[4] = calculateReduction($scope.amforaArrayData[2], $scope.amforaArrayData[3]);
     });
 });
